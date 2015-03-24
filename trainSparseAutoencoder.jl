b476bc527c914bf1a71305369cc9db6c82e01cce
Andrew Adare <andrewadare@gmail.com>
2015-03-24 14:57:01 -0600

    Backprop is not working yet.

--- trainSparseAutoencoder.jl
+++ trainSparseAutoencoder.jl
@@ -3,6 +3,7 @@ using Winston
 using NLopt
 
 include("dltUtils.jl")
+# include("pars.jl")
 
 function main()
     nv = 8*8                # Units in the visible layers. Equal to patch size.
@@ -10,12 +11,14 @@ function main()
     sparsity = 0.01         # Target mean activation of hidden units.
     lambda = 1e-4           # Weight decay (regularization) parameter.
     beta = 3                # Sparsity penalty weight.
+    npatches = 10000
 
     # 1. Create 10000 8x8 image patches from 10 nature scenes and view a few.
-    patches = sampleImages(patchsize = 8, npatches = 10000)
+    patches = sampleImages(patchsize = 8, npatches = npatches)
     if true
-        mosaic = imageMosaic(patches', 10)
-        figure(name="Image patches (10x10 sample)")
+        nshow = 15
+        mosaic = imageMosaic(patches', nshow)
+        figure(name="Image patches ($nshow x $nshow sample)")
         display(imagesc(mosaic))
         savefig("output/patches.png")
     end
@@ -23,14 +26,18 @@ function main()
     # 2. Sparse autoencoder objective function and its gradient wrt theta.
     # The weights W1,W2,b1,and b2 are unrolled into theta as a long vector.
     theta = initWeights(nv, nh)
+    println("Initialized random weights: ", mean(theta), " ", std(theta))
     J, grad = saeCost(theta, nv, nh, lambda, beta, sparsity, patches)
+
+    println("J = $J")
 
     # 3. Gradient checking
     # Compare "analytic" gradient with numerical approximation.
-    # Before running the AE test, reduce npatches above to 1000 or so.
-    println("Analytic vs numeric gradients for a small test problem:")
-    checkGradient()
-    if false
+    # Reduce npatches for this check, otherwise it is too slow.
+    if npatches <= 1000
+        println("Analytic vs numeric gradients for a small test problem:")
+        checkGradient()
+    
         println("Analytic vs numeric gradients for autoencoder function:")
         cost(t) = saeCost(t,nv,nh,lambda,beta,sparsity,patches)
         numgrad = numericalGradient(cost, theta)
@@ -48,29 +55,50 @@ function main()
     # e.g. :LD_MMA :LD_SLSQP :LN_SBPLX :LD_TNEWTON :LD_LBFGS
     # The first letter means Global (G) or Local (L).
     # The second letter means D: Derivative(s) required, N: No derivs. required. 
+    theta = initWeights(nv, nh)
     alg = :LD_LBFGS
     npars = length(theta)
     opt = Opt(alg, npars)
-    ftol_abs!(opt, 1e-5)
-    ftol_rel!(opt, 1e-4)
-    maxeval!(opt, 300)
-    lower_bounds!(opt, -10.0*ones(npars))
-    upper_bounds!(opt, +10.0*ones(npars))
+    # ftol_abs!(opt, 1e-6)
+    # ftol_rel!(opt, 1e-6)
+    # xtol_abs!(opt, 1e-4)
+    # xtol_rel!(opt, 1e-4)
+    maxeval!(opt, 400)
+    lower_bounds!(opt, -10*ones(npars))
+    upper_bounds!(opt, +10*ones(npars))
     println("Using ", algorithm_name(opt))
 
-    # Create a wrapper for the cost function acceptable to NLopt
+    # Wrap the cost function to match the signature expected by NLopt
+    ncalls = 0
     function f(x::Vector, grad::Vector)
-        J,grad = saeCost(x,nv,nh,lambda,beta,sparsity,patches)
+        J, grad[:] = saeCost(x,nv,nh,lambda,beta,sparsity,patches)
+        
+        ncalls += 1
+        ng = norm(grad)
+        println("f_$ncalls = $J, grad = $ng")
+        
         J
     end
 
     min_objective!(opt, f)
-
-    # min_objective!(opt, (theta, grad) -> lrCost!(theta, X, t, lambda, grad))
     (minCost, optTheta, status) = optimize!(opt, zeros(npars))        
     println("Cost = $minCost (returned $status)")
-
-
+    
+    viewW1(optTheta, nh, nv)
 end
+
+function viewW1(theta, nh, nv)
+    # As input try theta = trainedTheta() from pars.jl
+    
+    W1 = reshape(theta[1:nh*nv], nh, nv)
+    figure(name="W1 matrix from trained theta")
+    display(imagesc(W1))
+    
+    A = tileColumns(W1')
+    figure(name="Autoencoder weights")
+    display(imagesc(A))
+    # savefig("output/W1.png")
+end
+
 
 main()
