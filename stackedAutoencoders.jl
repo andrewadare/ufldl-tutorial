25adcf28bf56d1fd19e604283c126ea7dbae0cfc
Andrew Adare <andrewadare@gmail.com>
2015-04-04 15:12:35 -0600

    Add option to skip layerwise pretraining if weights are already stored (saves about an hour)

--- stackedAutoencoders.jl
+++ stackedAutoencoders.jl
@@ -11,7 +11,8 @@ function main()
     lambda = 3e-3             # Regularization parameter
     sparsity = 0.1            # Avg activation of hidden units
     beta = 3                  # Weight of sparsity penalty term
-    aeTrainIter = 2          # Number of L-BFGS iterations for AE training
+    aeTrainIter = 400         # Number of L-BFGS iterations for AE training
+    computeWeights = false
 
     # Get handwritten digit images and their labels from MNIST package.
     x, y = traindata()        # 784x60000, 60000 
@@ -25,30 +26,34 @@ function main()
     y[findin(y, [0.0])] = 10.0
     ytest[findin(ytest, [0.0])] = 10.0
 
-    tic()
-    println("Training sparse autoencoder 1...")
-    sae1Theta = initWeights(nin, nh1)
-    sae1OptTheta, optJ1, status1 = 
-    trainAutoencoder(sae1Theta,nin,nh1,lambda,beta,sparsity,x; 
-                     maxIter=aeTrainIter)
-    toc()
+    if computeWeights
+        println("Training sparse autoencoder 1...")
+        sae1Theta = initWeights(nin, nh1)
+        @time sae1OptTheta, optJ1, status1 =
+        trainAutoencoder(sae1Theta,nin,nh1,lambda,beta,sparsity,x;
+                         maxIter=aeTrainIter)
+        println("Feed-forward pass to compute features from autoencoder 1...")
+        sae1Features = feedForwardAutoencoder(sae1OptTheta, nin, nh1, x)
 
-    println("Feed-forward pass to compute features from autoencoder 1...")
-    sae1Features = feedForwardAutoencoder(sae1OptTheta, nin, nh1, x)
+        println("Training sparse autoencoder 2...")
+        sae2Theta = initWeights(nh1, nh2)
+        @time sae2OptTheta, optJ2, status2 =
+        trainAutoencoder(sae2Theta,nh1,nh2,lambda,beta,sparsity,sae1Features;
+                         maxIter=aeTrainIter)
+        println("Feed-forward pass to compute features from autoencoder 2...")
+        sae2Features = feedForwardAutoencoder(sae2OptTheta, nh1, nh2, sae1Features)
 
-    tic()
-    println("Training sparse autoencoder 2...")
-    sae2Theta = initWeights(nh1, nh2)
-    sae2OptTheta, optJ2, status2 = 
-    trainAutoencoder(sae2Theta,nh1,nh2,lambda,beta,sparsity,sae1Features; 
-                     maxIter=aeTrainIter)
-    toc()
+        println("Training softmax classifier on stacked autoencoder output...")
+        saeSoftmaxOptTheta, _, _ = trainSoftmax(nh2, ncat, sae2Features, y, lambda)
 
-    println("Feed-forward pass to compute features from autoencoder 2...")
-    sae2Features = feedForwardAutoencoder(sae2OptTheta, nh1, nh2, sae1Features)
+        writedlm("output/ae1pars.txt", sae1OptTheta)
+        writedlm("output/ae2pars.txt", sae2OptTheta)
+        writedlm("output/softmaxpars.txt", saeSoftmaxOptTheta)
+    end
 
-    println("Training softmax classifier on stacked autoencoder output...")
-    saeSoftmaxOptTheta, _, _ = trainSoftmax(nh2, ncat, sae2Features, y, lambda)
+    sae1OptTheta = readdlm("output/ae1pars.txt")
+    sae2OptTheta = readdlm("output/ae2pars.txt")
+    saeSoftmaxOptTheta = readdlm("output/softmaxpars.txt")
 
     # Up to this point, we have called functions from previous exercises. Now 
     # for some new code.
@@ -58,16 +63,22 @@ function main()
                          sae1OptTheta[2*nin*nh1+1:2*nin*nh1+nh1])
     stack[2] = AEWeights(reshape(sae2OptTheta[1:nh1*nh2], nh2, nh1),
                          sae2OptTheta[2*nh1*nh2+1:2*nh1*nh2+nh2])
-
     stackpars, arch = stack2Pars(stack)
     println("$(arch.inputSize) input units. Hidden layer sizes:")
     display(arch.layerSizes)
 
     stackedAeTheta = [saeSoftmaxOptTheta; stackpars]
 
-    size(stackedAeTheta)
-    trainStackedAutoencoder(stackedAeTheta, nin, nh2, ncat, arch, lambda,
-                            x, y; aeTrainIter)
+    stackedAeOptTheta, saecost =
+    trainStackedAutoencoder(stackedAeTheta[:], nin, nh2, ncat, arch, lambda,
+                            x, y; maxIter=12)
+
+    println("stackpars          $(size(stackpars         ))")
+    println("stackedAeTheta     $(size(stackedAeTheta    ))")
+    println("stackedAeOptTheta  $(size(stackedAeOptTheta ))")
+    println("sae1OptTheta       $(size(sae1OptTheta      ))")
+    println("sae2OptTheta       $(size(sae2OptTheta      ))")
+    println("saeSoftmaxOptTheta $(size(saeSoftmaxOptTheta))")
 end
 
-main()
+@time main()
