4e811af570d3211a38627f316891ed115242eefd
Andrew Adare <andrewadare@gmail.com>
2015-04-12 09:43:29 -0600

    Factor tests to separate functions

--- cnn.jl
+++ cnn.jl
@@ -12,60 +12,67 @@ const nv = patchDim^2 * nChannels
 const nh = 400
 const epsilon = 0.1
 
-function main()
-    optTheta  = readdlm("data/stl10features.txt")
-    meanPatch = readdlm("data/patch_means.txt")
-    zcaWhite  = readdlm("data/zcawhite.txt")
+# Load paramaters that were saved in linearDecoder.jl
+const optTheta  = readdlm("data/stl10features.txt")
+const meanPatch = readdlm("data/patch_means.txt")
+const zcaWhite  = readdlm("data/zcawhite.txt")
+const W = reshape(optTheta[1:nv * nh], nh, nv)
+const b = optTheta[2*nh*nv+1:2*nh*nv+nh]
 
-    W = reshape(optTheta[1:nv * nh], nh, nv)
-    b = optTheta[2*nh*nv+1:2*nh*nv+nh]
-    displayColorNetwork((W*zcaWhite)')
+# Load training and test data (provided as part of UFLDL tutorial)
+const trainDataFile = matopen("data/stlTrainSubset.mat")
+const testDataFile  = matopen("data/stlTestSubset.mat")
+const trainLabels   = read(trainDataFile, "trainLabels")
+const testLabels    = read(testDataFile, "testLabels")
+const trainImages   = read(trainDataFile, "trainImages")
+const testImages    = read(testDataFile, "testImages")
+const nTrain        = read(trainDataFile, "numTrainImages") # 2000 images
+const nTest         = read(testDataFile, "numTestImages")   # 3200 images
 
-    imFile = matopen("data/stlTrainSubset.mat") # List using names(imFile)
-    trainLabels = read(imFile, "trainLabels")
-    trainImages = read(imFile, "trainImages")
-    nTrain = read(imFile, "numTrainImages")     # 2000
-
-    # Test convolution code on the first 8 images 
+function testConvolution()
+    # Test cnnConvolve function on the first 8 training images. 
     # (convImages is 64 x 64 x 3 x 8).
     convImages = trainImages[:, :, :, 1:8]
-    # for i = 1:8
-    #     view(convImages[:,:,:,i])
-    # end
+    for i = 1:8
+        view(convImages[:,:,:,i])
+    end
+    convolvedFeatures = 
+    cnnConvolve(patchDim, nh, convImages, W, b, zcaWhite, meanPatch)
 
-    convolvedFeatures = cnnConvolve(patchDim, nh, convImages, W, b, zcaWhite, meanPatch)
-
-    # Test the cnnConvolve function
-    for i = 1:1 # Change to 1:1000 to run test
+    nCorrect = 0
+    for i = 1:1000
         fNum  = rand([1:nh;])
         imNum = rand([1:8;])
         imRow = rand([1:imgDim - patchDim + 1;])
         imCol = rand([1:imgDim - patchDim + 1;])    
-     
+ 
         patch = convImages[imRow:imRow + patchDim - 1, 
                            imCol:imCol + patchDim - 1, :, imNum]
         patch = patch[:]            
         patch = patch - meanPatch
         patch = zcaWhite * patch
-      
+  
         features = feedForwardAutoencoder(optTheta, nv, nh, patch)
         diff = features[fNum, 1] - convolvedFeatures[fNum, imNum, imRow, imCol]
-        println("Convolved feature - autoencoder activation: $diff")
         if abs(diff) > 1e-9
-            println("Convolved feature does not match activation from autoencoder")
+            error("Convolved feature != autoencoder activation")
             println("Feature Number    $fNum")
             println("Image Number      $imNum")
             println("Image Row         $imRow")
             println("Image Column      $imCol")
-            println("Convolved feature $(convolvedFeatures(fNum, imNum, imRow, imCol)))")
+            print("Convolved feature ")
+            println("$(convolvedFeatures(fNum, imNum, imRow, imCol)))")
             println("Sparse AE feature $(features(fNum, 1)))")
-            error("Convolved feature does not match activation from autoencoder")
-        end 
+        else
+            nCorrect += 1
+        end
     end
+    println("$nCorrect/1000 convolved features match autoencoder activations.")
+    convolvedFeatures
+end
 
+function testPooling(convolvedFeatures)
     pooledFeatures = cnnPool(poolDim, convolvedFeatures)
-
-    # Test pooling function
     testMatrix = reshape(1:64., 8, 8)
     expectedMatrix = [mean(mean(testMatrix[1:4, 1:4])) 
                       mean(mean(testMatrix[5:8, 1:4]))
@@ -82,8 +89,15 @@ function main()
         println("Got")
         println(pooledFeatures)
     end
+end
 
-    # TODO: run convolution and pooling on data.
+function main()
+    if false
+        displayColorNetwork((W*zcaWhite)')
+        tcFeatures = testConvolution()
+        testPooling(tcFeatures)
+    end
+
 end
 
 main()
